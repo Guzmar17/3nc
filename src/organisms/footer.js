fd683b8d085906f586b6aeabb3d9f31614c129e9
Kindex <miguelangelvera69@gmail.com>
2024-03-06 20:54:35 -0400

    creacion del banner principal

--- src/organisms/footer.js
+++ src/organisms/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-orange-600 sm:bg-blue-800 md:bg-red-800  lg:bg-yellow-800">
         <div className=" h-28">
-            <p> holaaaa</p>
+            <p> holaaaa xd</p>
         </div>
     </footer >
   )
