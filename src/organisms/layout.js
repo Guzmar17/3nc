162a24487de609b5856eff60079bef7ae716dcf4
Abraham Guzman <abraham030517@gmail.com>
2024-03-06 20:13:37 -0500

    Header

--- src/organisms/layout.js
+++ src/organisms/layout.js
@@ -1,11 +1,13 @@
 import './../App.css';
 import Footer from './footer'
+import Header from './header'
 
 export default function Layout({
     children,
   })  {
     return (
     <html>
+        <Header/>
         <div className="App bg-blue-200 min-h-screen flex flex-col justify-center items-center">
         {children}
         </div>
